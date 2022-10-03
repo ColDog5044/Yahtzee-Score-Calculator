1c473a231ec099e0c4fa034e0ef3b57ebdf581fc
Collin Laney <cman504430@gmail.com>
2022-10-02 20:40:27 -0400

    Initial Commit

---
+++ YahtzeeCalucator.ps1
@@ -0,0 +1,35 @@
+# Yahtzee Calculator
+# Made by Collin Laney (ColDog5044)
+# Date Modified: 10/2/2022
+
+
+
+# ==================
+# START FUNCTIONS
+# ==================
+
+function main {
+
+    break
+
+}
+
+function setup {
+
+    break
+
+}
+
+function card {
+
+    break
+
+}
+
+
+
+# ==================
+# END FUNCTIONS
+# ==================
+
+main
